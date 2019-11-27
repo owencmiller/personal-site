670e158de68691966b8cff69d5494786ecaa7c68
Owen Miller <owenchristianmiller@gmail.com>
2019-11-27 01:01:50 -0500

    added build dir for github actions

---
+++ out/_next/static/webpack/static/development/pages/_error.js.169010ae37adc09261b5.hot-update.js
@@ -0,0 +1,181 @@
+webpackHotUpdate("static/development/pages/_error.js",{
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fowenmiller%2FCode%2Fpersonal_website%2Fpossible%2Fweb-profile%2Fpages%2F_error.js!./":
+/*!****************************************************************************************************************************************************************************************************************************!*\
+  !*** /usr/local/lib/node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fowenmiller%2FCode%2Fpersonal_website%2Fpossible%2Fweb-profile%2Fpages%2F_error.js ***!
+  \****************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/_error", function() {
+      var page = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/_error.js */ "./pages/_error.js", function() {
+          if(!next.router.components["/_error"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/_error.js */ "./pages/_error.js")
+          next.router.update("/_error", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=next%2Fdist%2Fpages%2F_error!./":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/dist/pages/_error.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/core-js/array/from.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/core-js/is-iterable.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/arrayWithoutHoles.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/assertThisInitialized.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/getPrototypeOf.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/iterableToArray.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/nonIterableSpread.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/possibleConstructorReturn.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/setPrototypeOf.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/toConsumableArray.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/@babel/runtime-corejs2/helpers/esm/typeof.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/core-js/library/fn/array/from.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/core-js/library/fn/is-iterable.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/core-js/library/modules/_create-property.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/core-js/library/modules/core.is-iterable.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/core-js/library/modules/es6.array.from.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/next-server/dist/lib/amp.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/next-server/dist/lib/amphtml-context.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/next-server/dist/lib/head-manager-context.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/next-server/dist/lib/head.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/next-server/dist/lib/side-effect.js":
+false,
+
+/***/ "../../../../../../usr/local/lib/node_modules/next/node_modules/next-server/head.js":
+false,
+
+/***/ "./pages/_error.js":
+/*!*************************!*\
+  !*** ./pages/_error.js ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/classCallCheck */ "./node_modules/@babel/runtime-corejs2/helpers/esm/classCallCheck.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/createClass */ "./node_modules/@babel/runtime-corejs2/helpers/esm/createClass.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/possibleConstructorReturn */ "./node_modules/@babel/runtime-corejs2/helpers/esm/possibleConstructorReturn.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/getPrototypeOf */ "./node_modules/@babel/runtime-corejs2/helpers/esm/getPrototypeOf.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/inherits */ "./node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! next/router */ "../../../../../../usr/local/lib/node_modules/next/dist/client/router.js");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_6__);
+
+
+
+
+
+
+
+
+var Error =
+/*#__PURE__*/
+function (_React$Component) {
+  Object(_babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__["default"])(Error, _React$Component);
+
+  function Error() {
+    Object(_babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__["default"])(this, Error);
+
+    return Object(_babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__["default"])(this, Object(_babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__["default"])(Error).apply(this, arguments));
+  }
+
+  Object(_babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__["default"])(Error, [{
+    key: "render",
+    value: function render() {
+      return null;
+    }
+  }], [{
+    key: "getInitialProps",
+    value: function getInitialProps(_ref) {
+      var res = _ref.res;
+
+      if (res) {
+        res.writeHead(302, {
+          Location: '/'
+        });
+        res.end();
+      } else {
+        next_router__WEBPACK_IMPORTED_MODULE_6___default.a.push('/');
+      }
+
+      return {};
+    }
+  }]);
+
+  return Error;
+}(react__WEBPACK_IMPORTED_MODULE_5___default.a.Component);
+
+/* harmony default export */ __webpack_exports__["default"] = (Error);
+
+/***/ }),
+
+/***/ 1:
+/*!*******************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fowenmiller%2FCode%2Fpersonal_website%2Fpossible%2Fweb-profile%2Fpages%2F_error.js ***!
+  \*******************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F_error&absolutePagePath=%2FUsers%2Fowenmiller%2FCode%2Fpersonal_website%2Fpossible%2Fweb-profile%2Fpages%2F_error.js! */"../../../../../../usr/local/lib/node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2FUsers%2Fowenmiller%2FCode%2Fpersonal_website%2Fpossible%2Fweb-profile%2Fpages%2F_error.js!./");
+
+
+/***/ })
+
+})
+//# sourceMappingURL=_error.js.169010ae37adc09261b5.hot-update.js.map
